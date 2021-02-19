0fbb8274c26721f60cffae2e2120a9cce3f739dc
AmiTurgemaN <turge91@gmail.com>
2021-02-19 13:53:09 +0200

    Added another proxy server

--- src/app/services/shared.service.ts
+++ src/app/services/shared.service.ts
@@ -1,24 +1,30 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ServerInfoDTO } from '../models/server-info-DTO';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServerInfoHttpService {
 
+  serverInfoUrl:string = 'https://user.wartale.com/api.asp?key=794701946544a3f6d9e42238cde2a551&action=get_server_info';
+  proxyUrls:string [] = 
+  [
+    'https://thingproxy.freeboard.io/fetch/',
+    'https://cors-anywhere.herokuapp.com/'
+  ];
+
   constructor(private httpClient: HttpClient) { }
 
   getServerInfoInfo(): Observable<ServerInfoDTO> {
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-    const serverInfoUrl = 'https://user.wartale.com/api.asp?key=794701946544a3f6d9e42238cde2a551&action=get_server_info';
-    return this.httpClient.get(proxyUrl + serverInfoUrl)
-    .pipe(
-      map((response: ServerInfoDTO) => {
+    return this.httpClient.get(this.proxyUrls[0] + this.serverInfoUrl)
+    .pipe(map((response: ServerInfoDTO) => {
         return response;
       })
+    
     );
   }
 }
